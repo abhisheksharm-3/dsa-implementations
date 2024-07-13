4d3c49c32ed66f0efbbb391612f686791f460518
Abhishek Sharma <abhitiku2003@gmail.com>
2024-07-13 22:20:51 +0530

    did dl and quiksort

--- Algo Course/kata-machine/src/day1/DoublyLinkedList.ts
+++ Algo Course/kata-machine/src/day1/DoublyLinkedList.ts
@@ -1,27 +1,131 @@
+type Node<T> = {
+    data: T;
+    next: Node<T> | null;
+    prev: Node<T> | null;
+};
+
 export default class DoublyLinkedList<T> {
     public length: number;
-
-    
+    private head: Node<T> | null;
+    private tail: Node<T> | null;
 
     constructor() {
+        this.length = 0;
+        this.head = null;
+        this.tail = null;
     }
 
     prepend(item: T): void {
+        const newNode: Node<T> = { data: item, next: this.head, prev: null };
+        if (this.head) {
+            this.head.prev = newNode;
+        } else {
+            this.tail = newNode; // If the list was empty
+        }
+        this.head = newNode;
+        this.length++;
+    }
 
-}
     insertAt(item: T, idx: number): void {
+        if (idx < 0 || idx > this.length) {
+            throw new Error("Index out of bounds");
+        }
 
-}
+        if (idx === 0) {
+            this.prepend(item);
+            return;
+        } else if (idx === this.length) {
+            this.append(item);
+            return;
+        }
+
+        let curr = this.head;
+        for (let i = 0; i < idx; i++) {
+            curr = curr!.next;
+        }
+
+        const newNode: Node<T> = { data: item, next: curr, prev: curr!.prev };
+        if (curr!.prev) {
+            curr!.prev.next = newNode;
+        }
+        curr!.prev = newNode;
+        this.length++;
+    }
+
     append(item: T): void {
+        const newNode: Node<T> = { data: item, next: null, prev: this.tail };
+        if (this.tail) {
+            this.tail.next = newNode;
+        } else {
+            this.head = newNode; // If the list was empty
+        }
+        this.tail = newNode;
+        this.length++;
+    }
 
-}
     remove(item: T): T | undefined {
+        let curr = this.head;
+        while (curr) {
+            if (curr.data === item) {
+                if (curr.prev) {
+                    curr.prev.next = curr.next;
+                } else {
+                    this.head = curr.next; // Removing the head
+                }
 
-}
+                if (curr.next) {
+                    curr.next.prev = curr.prev;
+                } else {
+                    this.tail = curr.prev; // Removing the tail
+                }
+
+                this.length--;
+                return curr.data;
+            }
+            curr = curr.next;
+        }
+        return undefined;
+    }
+
     get(idx: number): T | undefined {
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
 
-}
+        let curr = this.head;
+        for (let i = 0; i < idx; i++) {
+            curr = curr!.next;
+        }
+        return curr?.data;
+    }
+
     removeAt(idx: number): T | undefined {
+        if (idx < 0 || idx >= this.length) {
+            return undefined;
+        }
 
-}
+        let curr = this.head;
+        for (let i = 0; i < idx; i++) {
+            curr = curr!.next;
+        }
+
+        if (curr) {
+            if (curr.prev) {
+                curr.prev.next = curr.next;
+            } else {
+                this.head = curr.next; // Removing the head
+            }
+
+            if (curr.next) {
+                curr.next.prev = curr.prev;
+            } else {
+                this.tail = curr.prev; // Removing the tail
+            }
+
+            this.length--;
+            return curr.data;
+        }
+
+        return undefined;
+    }
 }
--- Algo Course/kata-machine/src/day1/QuickSort.ts
+++ Algo Course/kata-machine/src/day1/QuickSort.ts
@@ -1,3 +1,29 @@
-export default function quick_sort(arr: number[]): void {
+function quick_sort_helper(arr: number[], low: number, high: number): void {
+    if (low < high) {
+        let pi = partition(arr, low, high);
+        quick_sort_helper(arr, low, pi - 1);
+        quick_sort_helper(arr, pi + 1, high);
+    }
+}
 
+function partition(arr: number[], low: number, high: number): number {
+    let pivot = arr[high];
+    let i = low - 1;
+    for (let j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            let temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+    let temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+
+
+export default function quick_sort(arr: number[]): void {
+    quick_sort_helper(arr, 0, arr.length - 1);
 }
